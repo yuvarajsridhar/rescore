d1fbcac9849530d4be7277cf3dc7c0ecfc0a0692
yuvarajsridhar <syuvraj8@gmail.com>
2017-01-30 11:46:53 +0530

    select updated

--- src/main/java/com/yuvaraj/dao/MaxOrderDao.java
+++ src/main/java/com/yuvaraj/dao/MaxOrderDao.java
@@ -1,8 +1,13 @@
 package com.yuvaraj.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.yuvaraj.model.MaxOrder;
+import com.yuvaraj.model.SeedFood;
 import com.yuvaraj.util.ConnectionUtil;
 
 public class MaxOrderDao {
@@ -29,5 +34,23 @@ JdbcTemplate jdbcTemplate = ConnectionUtil.getJdbcTemplate();
 		int rows = jdbcTemplate.update(sql, max_order,id);
 		System.out.println("No of rows updated: " + rows);
 	}
+	public List<MaxOrder>list(){
+		 String sql="select *from max_order";
+		  return jdbcTemplate.query(sql, (rs, rowNum) -> {
+			 final  MaxOrder MaxOrder = convert(rs);
+			  return MaxOrder;
+					  
+		 });
+	}
+		  public MaxOrder convert(final ResultSet rs) throws SQLException {
+				MaxOrder maxOrder = new MaxOrder();
+
+				maxOrder.setId(rs.getInt("Id"));
+				maxOrder.setMaxOrder(rs.getInt("max_order"));
+		        maxOrder.setKey(rs.getString("key"));
+				
+				
+				return maxOrder;
+			}
 	
 }
--- src/test/java/com/yuvaraj/model/TestMaxOrderDao.java
+++ src/test/java/com/yuvaraj/model/TestMaxOrderDao.java
@@ -1,18 +1,36 @@
 package com.yuvaraj.model;
 
+import java.util.Iterator;
+import java.util.List;
+
 import com.yuvaraj.dao.MaxOrderDao;
 
 public class TestMaxOrderDao {
 
 	public static void main(String[] args) {
-		MaxOrder maxOrder = new MaxOrder();
+		
+	/*	MaxOrder maxOrder = new MaxOrder();
 		maxOrder.setId(2);
 		
 		maxOrder.setMaxOrder(6);
 		maxOrder.setKey("week day");
 		
 		MaxOrderDao maxOrderDao=new MaxOrderDao();
-		maxOrderDao.save(maxOrder);
+		maxOrderDao.save(maxOrder);*/
+		
+		MaxOrderDao maxOrderDao=new MaxOrderDao();
+		 final  List<MaxOrder> list = maxOrderDao.list();
+
+			final Iterator ii = list.iterator();
+			
+			while (ii.hasNext()) {
+				final MaxOrder maxOrder = (MaxOrder) ii.next();
+
+				System.out.println(maxOrder.getId());
+				System.out.println(maxOrder.getMaxOrder());
+				
+				System.out.println(maxOrder.getKey());
+			}
 
 	}
 
